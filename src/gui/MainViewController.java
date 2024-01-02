c452a7acb98de95b3416ff505a9e77ac41f0c4af
Ivan Maia <ivanleonardo_maia@hotmail.com>
2024-01-02 16:05:57 -0300

    Add keyCode.TAB attribute/method

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -19,74 +19,60 @@ import javafx.scene.input.KeyCode;
 import model.dao.impl.ProductDaoSQL;
 import model.entities.Product;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
-	@FXML
-	private TextField textCodProduct;
-	
-	@FXML
-	private Button buttonSearch;
-	
-	@FXML
-	private TableView<Product> tableView;
-	
-	@FXML
-	private TableColumn<Product, String> columnDescProduct;
-	
-	@FXML
-	private TableColumn<Product, String> columnDescManufacturer;
-	
-	
-	private ProductDaoSQL productDao;
-	
-	public MainViewController() {
-	}
-	
-	@SuppressWarnings("exports")
-	public void setProductDao(ProductDaoSQL productDao) {
-		this.productDao = productDao;
-	}
-	
-	@SuppressWarnings("exports")
-	public MainViewController(ProductDaoSQL productDao) {
-		this.productDao = productDao;
-	}
-	
-	@FXML
-	void onButtonSearch(ActionEvent ecent) {
-		try {
-			Long codProduct = Long.valueOf(textCodProduct.getText());
-			Product product = productDao.findByProduct(codProduct);
-			
-			ObservableList<Product> products = FXCollections.observableArrayList();
-			
-			if(codProduct != null && product != null) {
-				products.add(product);
-				tableView.setItems(products);
-			}else {
-				Alerts.showAlert("ATENÇÃO", "Produto não encontrado ou não Cadastrado.", "Verifique se o código digitado está correto. Caso contrário, entre em contato com o Suporte LE POSTICHE.", AlertType.INFORMATION);
-			}
-			
-		}catch (NumberFormatException e) {
-			Alerts.showAlert("ATENÇÃO", "Código Inválido", "O código digitado está fora de padrão", AlertType.INFORMATION);
-		}
-	}
+    @FXML
+    private TextField textCodProduct;
 
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		columnDescProduct.setCellValueFactory(new PropertyValueFactory<>("descProduct"));
+    @FXML
+    private Button buttonSearch;
+
+    @FXML
+    private TableView<Product> tableView;
+
+    @FXML
+    private TableColumn<Product, String> columnDescProduct;
+
+    @FXML
+    private TableColumn<Product, String> columnDescManufacturer;
+
+    private ProductDaoSQL productDao;
+
+    public MainViewController() {
+    }
+
+    @SuppressWarnings("exports")
+    public void setProductDao(ProductDaoSQL productDao) {
+        this.productDao = productDao;
+    }
+
+    @SuppressWarnings("exports")
+    public MainViewController(ProductDaoSQL productDao) {
+        this.productDao = productDao;
+    }
+
+    @FXML
+    void onButtonSearch(ActionEvent event) {
+        handleSearch();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    	columnDescProduct.setCellValueFactory(new PropertyValueFactory<>("descProduct"));
         columnDescManufacturer.setCellValueFactory(new PropertyValueFactory<>("descManufacturer"));
 
-       
         textCodProduct.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ENTER) {
+            if (event.getCode().equals(KeyCode.ENTER)) {
                 handleSearch();
+                event.consume(); // Evita que o evento se propague
             }
         });
 
         buttonSearch.setOnAction(event -> {
             handleSearch();
         });
+
+        textCodProduct.requestFocus(); // Define o foco inicial no campo de pesquisa
     }
 
     private void handleSearch() {
